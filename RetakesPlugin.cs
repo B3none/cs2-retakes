f961829cd7aac603735ae25648c197e625af8ceb
B3none <ablackham2000@gmail.com>
2023-12-19 17:56:42 +0000

    Updated module description

--- RetakesPlugin.cs
+++ RetakesPlugin.cs
@@ -9,7 +9,7 @@ public class RetakesPlugin : BasePlugin
     public override string ModuleName => "Retakes Plugin";
     public override string ModuleVersion => "0.0.1";
     public override string ModuleAuthor => "B3none";
-    public override string ModuleDescription => "Community retakes gamemode for CS2.";
+    public override string ModuleDescription => "Community retakes for CS2.";
     
     public override void Load(bool hotReload)
     {
