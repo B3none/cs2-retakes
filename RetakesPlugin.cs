e0110bf78ccb6dbea734001c50f21c7e59999280
B3none <ablackham2000@gmail.com>
2023-12-20 03:26:40 +0000

    Added teleport back, it's actually kinda useful for debugging lol

--- RetakesPlugin.cs
+++ RetakesPlugin.cs
@@ -99,6 +99,42 @@ public class RetakesPlugin : BasePlugin
         commandInfo.ReplyToCommand($"{MessagePrefix}Adding spawn.");
     }
     
+    [ConsoleCommand("css_teleport", "This command teleports the player to the given coordinates")]
+    [RequiresPermissions("@css/root")]
+    public void OnCommandTeleport(CCSPlayerController? player, CommandInfo command)
+    {
+        if (player == null)
+        {
+            return;
+        }
+        if (!player.PlayerPawn.IsValid)
+        {
+            return;
+        }
+
+        if (command.ArgCount != 4)
+        {
+            return;
+        }
+
+        if (!float.TryParse(command.ArgByIndex(1), out float positionX))
+        {
+            return;
+        }
+
+        if (!float.TryParse(command.ArgByIndex(2), out float positionY))
+        {
+            return;
+        }
+
+        if (!float.TryParse(command.ArgByIndex(3), out float positionZ))
+        {
+            return;
+        }
+
+        player?.PlayerPawn?.Value?.Teleport(new Vector(positionX, positionY, positionZ), new QAngle(0f,0f,0f), new Vector(0f, 0f, 0f));
+    }
+    
     // Listeners
     private void OnMapStartHandler(string mapName)
     {
@@ -108,6 +144,29 @@ public class RetakesPlugin : BasePlugin
             _mapConfig = new MapConfig(ModuleDirectory, Server.MapName);
             _mapConfig.Load();
         }
+    }
+
+    [GameEventHandler]
+    public HookResult OnWeaponFire(EventWeaponFire @event, GameEventInfo info)
+    {
+        Console.WriteLine($"{MessagePrefix}OnPlayerSpawn event fired for {@event.Userid.PlayerName}");
+
+        var player = @event.Userid;
+        var weapon = @event.Weapon;
+        
+        var buttons = @event.Userid.Buttons;
+
+        if (_gameRules.FreezePeriod)
+        {
+            
+        }
+        
+        if((buttons & PlayerButtons.Use) != 0)
+        {
+            //do something
+        }
+        
+        return HookResult.Continue;
     }
 
     [GameEventHandler]
