cadb42b9f654a892a438f4b4c725c44476cf2794
B3none <ablackham2000@gmail.com>
2023-12-29 02:49:57 +0000

    Updated retakes config data property for enabling fallback allocation

--- Modules/Config/RetakesConfigData.cs
+++ Modules/Config/RetakesConfigData.cs
@@ -8,5 +8,5 @@ public class RetakesConfigData
     public int MaxPlayers { get; set; } = 9;
     public float TerroristRatio { get; set; } = 0.45f;
     public int RoundsToScramble { get; set; } = 5;
-    public bool EnableAllocation { get; set; } = true;
+    public bool EnableFallbackAllocation { get; set; } = true;
 }
