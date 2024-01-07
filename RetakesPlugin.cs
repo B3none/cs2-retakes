e19d5bfe68443f72ead1c73d5b9425244d7c04a4
B3none <ablackham2000@gmail.com>
2024-01-07 20:07:59 +0000

    Bumped version to 1.1.9

--- RetakesPlugin.cs
+++ RetakesPlugin.cs
@@ -17,7 +17,7 @@ namespace RetakesPlugin;
 [MinimumApiVersion(131)]
 public class RetakesPlugin : BasePlugin
 {
-    private const string Version = "1.1.8";
+    private const string Version = "1.1.9";
     
     public override string ModuleName => "Retakes Plugin";
     public override string ModuleVersion => Version;
