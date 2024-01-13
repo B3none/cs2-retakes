ababca728c02f31fa40915ff3a4170ab208b1b1d
B3none <ablackham2000@gmail.com>
2024-01-13 19:28:17 +0000

    Bumped retakes plugin version to 1.2.8

--- RetakesPlugin.cs
+++ RetakesPlugin.cs
@@ -18,7 +18,7 @@ namespace RetakesPlugin;
 [MinimumApiVersion(131)]
 public class RetakesPlugin : BasePlugin
 {
-    private const string Version = "1.2.7";
+    private const string Version = "1.2.8";
     
     #region Plugin info
     public override string ModuleName => "Retakes Plugin";
