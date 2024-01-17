4644308d552cd687fc03b80ad2f135d7adb1e30f
B3none <ablackham2000@gmail.com>
2024-01-17 01:35:28 +0000

    Bumped version to 1.3.7

--- RetakesPlugin.cs
+++ RetakesPlugin.cs
@@ -18,7 +18,7 @@ namespace RetakesPlugin;
 [MinimumApiVersion(147)]
 public class RetakesPlugin : BasePlugin
 {
-    private const string Version = "1.3.6";
+    private const string Version = "1.3.7";
     
     #region Plugin info
     public override string ModuleName => "Retakes Plugin";
