2515aeec1be62a89fe2081fd06d0c1bac682f0ab
B3none <ablackham2000@gmail.com>
2024-01-23 16:52:49 +0000

    Updated addspawn command

--- RetakesPlugin.cs
+++ RetakesPlugin.cs
@@ -114,7 +114,7 @@ public class RetakesPlugin : BasePlugin
     }
     
     [ConsoleCommand("css_addspawn", "Adds a retakes spawn point to the map for the bombsite currently shown.")]
-    [CommandHelper(minArgs: 1, usage: "[T/CT]", whoCanExecute: CommandUsage.CLIENT_ONLY)]
+    [CommandHelper(minArgs: 1, usage: "[T/CT] [Y/N can be planter]", whoCanExecute: CommandUsage.CLIENT_ONLY)]
     [RequiresPermissions("@css/root")]
     public void OnCommandAddSpawn(CCSPlayerController? player, CommandInfo commandInfo)
     {
@@ -143,13 +143,14 @@ public class RetakesPlugin : BasePlugin
             return;
         }
         
-        var spawns = _spawnManager.GetSpawns((Bombsite)_showingSpawnsForBombsite);
-        
-        if (spawns.Count == 0)
+        var canBePlanterInput = commandInfo.GetArg(2).ToUpper();
+        if (!string.IsNullOrWhiteSpace(canBePlanterInput) && canBePlanterInput != "Y" && canBePlanterInput != "N")
         {
-            commandInfo.ReplyToCommand($"{MessagePrefix}No spawns found.");
+            commandInfo.ReplyToCommand($"{MessagePrefix}Incorrect value passed for can be a planter [Y / N] - [Value: {canBePlanterInput}].");
             return;
         }
+        
+        var spawns = _spawnManager.GetSpawns((Bombsite)_showingSpawnsForBombsite);
         
         var closestDistance = 9999.9;
 
@@ -177,7 +178,7 @@ public class RetakesPlugin : BasePlugin
         )
         {
             Team = team == "T" ? CsTeam.Terrorist : CsTeam.CounterTerrorist,
-            CanBePlanter = team == "T" && player.PlayerPawn.Value.InBombZone,
+            CanBePlanter = team == "T" && !string.IsNullOrWhiteSpace(canBePlanterInput) ? canBePlanterInput == "Y" : player.PlayerPawn.Value.InBombZone,
             Bombsite = (Bombsite)_showingSpawnsForBombsite
         };
         Helpers.ShowSpawn(newSpawn);
