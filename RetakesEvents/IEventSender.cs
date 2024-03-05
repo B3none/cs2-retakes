6b0814102ba5e0806aae3f44ed0624793f36c992
B3none <ablackham2000@gmail.com>
2024-03-05 22:49:49 +0000

    Begin restructure for event firing

---
+++ RetakesEvents/IEventSender.cs
@@ -0,0 +1,22 @@
+﻿namespace RetakesEvents.Contracts;
+
+public interface IEventSender
+{
+    event EventHandler<PluginEventArgs> PluginEventOccurred;
+
+    void CheckEventListeners();
+    void AddEventListener(EventHandler<PluginEventArgs> listener);
+    void TriggerEvent(string eventName, object eventData);
+}
+
+public class PluginEventArgs : EventArgs
+{
+    public string EventName { get; }
+    public object EventData { get; }
+
+    public PluginEventArgs(string eventName, object eventData)
+    {
+        EventName = eventName;
+        EventData = eventData;
+    }
+}
