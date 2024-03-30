5342b0434960d6e7b64dbfb529c24dfb1fa4cf16
B3none <24966460+B3none@users.noreply.github.com>
2024-03-30 23:42:59 +0100

    Bumped version to 2.0.1

--- RetakesPlugin/RetakesPlugin.cs
+++ RetakesPlugin/RetakesPlugin.cs
@@ -20,7 +20,7 @@ namespace RetakesPlugin;
 [MinimumApiVersion(201)]
 public class RetakesPlugin : BasePlugin
 {
-    private const string Version = "2.0.0";
+    private const string Version = "2.0.1";
 
     #region Plugin info
     public override string ModuleName => "Retakes Plugin";
