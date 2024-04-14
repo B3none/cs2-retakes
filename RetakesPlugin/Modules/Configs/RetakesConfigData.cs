9eb332ad92d4b0da1289ee4ea5227fb060d36b33
B3none <ablackham2000@gmail.com>
2024-04-14 16:33:35 +0100

    Removed MinPlayers from RetakesConfigData

--- RetakesPlugin/Modules/Configs/RetakesConfigData.cs
+++ RetakesPlugin/Modules/Configs/RetakesConfigData.cs
@@ -5,7 +5,6 @@ public class RetakesConfigData
     public static int CurrentVersion = 9;
 
     public int Version { get; set; } = CurrentVersion;
-    public int MinPlayers { get; set; } = 1;
     public int MaxPlayers { get; set; } = 9;
     public float TerroristRatio { get; set; } = 0.45f;
     public int RoundsToScramble { get; set; } = 5;
