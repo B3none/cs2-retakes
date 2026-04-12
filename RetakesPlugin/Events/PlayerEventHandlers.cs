7cb25ff1b5356208a709833d7dee43d8d31eac52
Ravid-A <ravid2903@gmail.com>
2026-04-12 16:52:18 +0300

    feat: add a new config value to enable the auto move to spectator

--- RetakesPlugin/Events/PlayerEventHandlers.cs
+++ RetakesPlugin/Events/PlayerEventHandlers.cs
@@ -31,6 +31,20 @@ public class PlayerEventHandlers
 
         player.ForceTeamTime = 3600.0f;
 
+        if(_plugin.Config.Queue.ShouldAutoJoinSpectators)
+        {
+            _plugin.AddTimer(1.0f, () =>
+            {
+                if (!PlayerHelper.IsValid(player))
+                {
+                    return;
+                }
+
+                player.ChangeTeam(CsTeam.Spectator);
+                player.ExecuteClientCommand("teammenu");
+            });
+        }
+
         // Grant VIP to contributors
         if (new List<ulong> { 76561198028510846, 76561198044886803, 76561198414501446, 76561199074660131 }.Contains(player.SteamID))
         {
